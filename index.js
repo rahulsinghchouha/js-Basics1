c54c468e0edef6d4e919e30771ef00032c456a19
rahulsinghchouha <singhrahul15737@gmail.com>
2024-02-29 16:30:21 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,95 @@
+
+console.log('jay mata di');
+   
+// let v=('rahul');
+
+// console.log(v);
+// let c=true;
+
+// console.log(c);
+// let d=12.5;
+
+// console.log(d);
+// let a=5;
+
+// console.log(a);
+// var z=20;
+// var z=10;
+// console.log(z);
+
+// const num=12;
+ 
+// let lname=3;
+// lname="ghee";
+
+// console.log(lname);
+
+// let ar=['rahul','58'];
+// console.log(ar[6]);
+
+
+// let a=9;
+// let b=89;
+
+// let c=a+b;
+// console.log(c);
+
+// let x=9;
+// console.log(x++);
+
+// let a=1;
+// let b='1';
+// //console.log(a==b);
+
+// console.log(a===b);
+
+// let age=18;
+// let stats= (age>=18)?'you con vote':'you cant' ;
+// console.log(stats);
+
+
+// console.log(false||'love');
+
+// console.log(true||'love');
+
+// console.log(false&&'love');
+// console.log(5|8);
+
+// let a=48;
+// if (a>90) {
+//     console.log('A');
+// }
+// else if(a>=80)
+// {
+//     console.log('B');
+// }
+// else if(a>=70)
+// {
+//     console.log('C');
+// }
+// else{
+//     console.log('fail');
+// }
+
+let a=2;
+
+switch (a) {
+    case 1:console.log('1');
+    break;
+    case 2:console.log('2');
+        
+    break;
+
+    default:console.log('default');
+    
+}
+
+for (let index = 0; index < 5; index++) {
+    console.log('jay mata di');
+    
+}
+
+let i=1;
+do{
+    console.log('1 time');
+}while(i<1);
